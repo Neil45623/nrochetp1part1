43f941c2a37a4c1e049de4e753cf89f1a02af690
Neil45623 <86229955+Neil45623@users.noreply.github.com>
2022-01-04 16:08:20 +0100

    Update snifer.js

--- snifer.js
+++ snifer.js
@@ -1,1 +1,18 @@
-t
+const express = require("express");
+
+const app = express();
+
+app.get('/ping', (_demande, reponse) => {
+    reponse.json('pong');
+});
+
+const port = getRandomPort();
+console.log(port);
+
+app.listen(port);
+
+function getRandomPort(min, max) {
+    min = Math.ceil(3000);
+    max = Math.floor(4000);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
